4ccf59713b179932fe12244734d336d492a6a0a5
Ville Mäkilä <ville.makila@protonmail.com>
2021-06-17 13:46:53 +0300

    created back-button

---
+++ components/BackButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <div @click="to">
+    {{ label }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BackButton',
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    to() {
+      this.$router.go(-1)
+    },
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+div {
+  @apply max-w-max p-2 m-1 bg-gray-200 rounded-lg text-3xl
+  hover:bg-gray-300 cursor-pointer;
+}
+</style>
