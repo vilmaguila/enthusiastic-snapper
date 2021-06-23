7a3e84dcd02358c6a55ce2419c1af3f13229452f
Ville Mäkilä <ville.makila@protonmail.com>
2021-06-23 14:46:47 +0300

    replaced authentication with nuxt/auth + small improvements

---
+++ components/CoolButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CoolButton',
+}
+</script>
+
+<style lang="postcss" scoped>
+div {
+  @apply max-w-max p-2 m-1 bg-gray-200 rounded-lg text-3xl
+  hover:bg-gray-300 cursor-pointer;
+}
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>
+      You are trying to access resource that required authentication, please
+      login!
+    </h1>
+    <cool-button @click.native="login">Login</cool-button>
+    <back-button label="Back"></back-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  methods: {
+    login() {
+      this.$auth.loginWith('auth0')
+    },
+  },
+}
+</script>
